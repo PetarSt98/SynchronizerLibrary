b0e0a47e9f83338440601822dd91a7ceff8ddf79
pstojkov <petar.stojkovic@cern.ch>
2023-05-12 11:25:40 +0200

    Test commit

--- Class1.cs
+++ Class1.cs
@@ -1,5 +1,10 @@
-﻿namespace SynchronizerLibrary;
+﻿using System;
+
+namespace SynchronizerLibrary;
 public class Class1
 {
-
+    public void printara()
+    {
+        Console.WriteLine("CAO");
+    }
 }
