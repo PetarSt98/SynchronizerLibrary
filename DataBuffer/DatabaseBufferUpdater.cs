f49a71592981bfa81ac70d45b1788af1451c1bac
pstojkov <petar.stojkovic@cern.ch>
2023-09-21 11:04:33 +0200

    Added case when it is uncompleted sync, to put exception flag

--- DataBuffer/DatabaseBufferUpdater.cs
+++ DataBuffer/DatabaseBufferUpdater.cs
@@ -58,6 +58,7 @@ namespace SynchronizerLibrary.DataBuffer
                     {
                         if (sendEmail)
                         {
+                            bool uncompletedSync = false;
                             try
                             {
                                 Console.WriteLine("Sending email");
@@ -87,20 +88,22 @@ namespace SynchronizerLibrary.DataBuffer
                                 {
                                     Console.WriteLine("Status");
                                     Console.WriteLine(obj.StatusMessage);
-                                    
+
                                     if (obj.StatusMessage == "Access is denied.")
                                     {
                                         subject = "no-reply Remote Desktop Service device synchronization Uncompleted";
                                         template = System.IO.File.ReadAllText(@".\DataBuffer\EmailTemplates\User_RequestUncompleted.htm");  // Replace with actual path
                                         template = template.Replace("$reason", "TS Gateway does not have access to your device.");
                                         template = template.Replace("$raison", "TS Gateway n'a pas accès à votre appareil.");
+                                        uncompletedSync = true;
                                     }
                                     else if (obj.StatusMessage == "There is no such object on the server.")
                                     {
                                         subject = "no-reply Remote Desktop Service device synchronization Uncompleted";
                                         template = System.IO.File.ReadAllText(@".\DataBuffer\EmailTemplates\User_RequestUncompleted.htm");  // Replace with actual path
-                                        template = template.Replace("$reason", "The device cannot be found on the server.");
+                                        template = template.Replace("$reason", $"The device {remoteMachine} cannot be found on the server.");
                                         template = template.Replace("$raison", "L'appareil n'est pas trouvé sur le serveur.");
+                                        uncompletedSync = true;
                                     }
                                     else if (obj.StatusMessage.Contains("disabled"))
                                     {
@@ -108,6 +111,7 @@ namespace SynchronizerLibrary.DataBuffer
                                         template = System.IO.File.ReadAllText(@".\DataBuffer\EmailTemplates\User_RequestUncompleted.htm");  // Replace with actual path
                                         template = template.Replace("$reason", "Administrator user in Local User and Groups on your machine is disabled.");
                                         template = template.Replace("$raison", "L'utilisateur administrateur est désactivé dans le groupe local d'utilisateurs et de groupes de votre machine.");
+                                        uncompletedSync = true;
                                     }
                                     else if (obj.StatusMessage == "Device is unreachable.")
                                     {
@@ -115,6 +119,7 @@ namespace SynchronizerLibrary.DataBuffer
                                         template = System.IO.File.ReadAllText(@".\DataBuffer\EmailTemplates\User_RequestUncompleted.htm");  // Replace with actual path
                                         template = template.Replace("$reason", "Your device is not reachable by TS Gateway.");
                                         template = template.Replace("$raison", "Votre appareil n'est pas accessible par TS Gateway.");
+                                        uncompletedSync = true;
                                     }
                                     else if (obj.StatusMessage == "The network path was not found.")
                                     {
@@ -122,6 +127,7 @@ namespace SynchronizerLibrary.DataBuffer
                                         template = System.IO.File.ReadAllText(@".\DataBuffer\EmailTemplates\User_RequestUncompleted.htm");  // Replace with actual path
                                         template = template.Replace("$reason", "Your device was offline when TS Gateway tried to reach it.");
                                         template = template.Replace("$raison", "Votre appareil était hors ligne lorsque TS Gateway a essayé de le joindre.");
+                                        uncompletedSync = true;
                                     }
                                     else
                                     {
@@ -142,8 +148,11 @@ namespace SynchronizerLibrary.DataBuffer
                                 // Now use the template as the body of your email
                                 string toAddress = obj.GroupName.Replace("LG-", "") + "@cern.ch";
                                 //string toAddressCC = resource.resourceOwner.Replace(@"CERN\", "") + "@cern.ch";
-                                string toAddressCC = deviceInfo["ResponsiblePersonUsername"] + "@cern.ch";
-                                
+                                string toAddressCC = null;
+                                if (deviceInfo["ResponsiblePersonUsername"].Length != 0)
+                                {
+                                    toAddressCC = deviceInfo["ResponsiblePersonUsername"] + "@cern.ch";
+                                }
                                 string body = template;
 
                                 Console.WriteLine(subject);
@@ -155,9 +164,34 @@ namespace SynchronizerLibrary.DataBuffer
                                     Console.WriteLine("Email sent");
                                 }
 
-                                var cacheData = new SpamFailureHandler(remoteMachine, users);
 
-                                cacheData.CacheSpam();
+                                if (uncompletedSync)
+                                {
+                                    var matchingRapsUncompleted = db.raps
+                                        .Where(r => r.resourceGroupName == obj.GroupName)
+                                        .Include(r => r.rap_resource)  // Including related rap_resources
+                                        .ToList();
+
+                                    // Filtering unsynchronized raps and their resources
+                                    var unsynchronizedRapsUncompleted = matchingRapsUncompleted
+                                        .Where(r => r.rap_resource.Any(rr => !rr.synchronized && string.Equals(rr.resourceName, obj.ComputerName, StringComparison.OrdinalIgnoreCase) && !rr.toDelete))
+                                        .ToList();
+                                    foreach (var unsynchronizedRap in unsynchronizedRapsUncompleted)
+                                    {
+                                        foreach (var resource in unsynchronizedRap.rap_resource.Where(rr => string.Equals(rr.resourceName, obj.ComputerName, StringComparison.OrdinalIgnoreCase)))
+                                        {
+                                            resource.synchronized = true;
+                                            resource.exception = true;
+                                        }
+                                    }
+                                    SpamFailureHandler.CleanCache(remoteMachine, users);
+                                }
+                                else
+                                {
+                                    var cacheData = new SpamFailureHandler(remoteMachine, users);
+                                    cacheData.CacheSpam();
+                                }
+
                                 //if (!pair.partial.Value)
                                 //{
                                 //    var rapResourcesToDelete = db.rap_resource.Where(rr => (rr.RAPName == obj.GroupName.Replace("LG-", "RAP_") && string.Equals(rr.resourceName, obj.ComputerName, StringComparison.OrdinalIgnoreCase))).ToList();
