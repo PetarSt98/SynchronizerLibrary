e0dd424a8024d89d576cff010ee8e197d6d2a29f
pstojkov <petar.stojkovic@cern.ch>
2023-10-17 23:37:26 +0200

    Updated cacher to be more robust

--- CommonServices/GatewayRapSynchronizer.cs
+++ CommonServices/GatewayRapSynchronizer.cs
@@ -36,12 +36,13 @@ namespace SynchronizerLibrary.CommonServices
                 throw;
             }
         }
+
         public List<string> GetRapNamesAsync(string serverName, bool cacheFlag = false)
         {
             if (cacheFlag)
             {
                 Console.WriteLine($"Using cached Gateway Policies");
-                return Cacher.LoadCacheFromFile();
+                return Cacher.LoadPolicyCacheFromFile();
             }
             else
             {
@@ -100,7 +101,7 @@ namespace SynchronizerLibrary.CommonServices
                     }
                     LoggerSingleton.SynchronizedRaps.Info($"Finished querying RAP/Policy names from gateway '{serverName}'.");
 
-                    Cacher.SaveCacheToFile(rapNames);
+                    Cacher.SavePolicyCacheToFile(rapNames);
                     return rapNames;
                 }
                 catch (Exception ex)
@@ -371,7 +372,5 @@ namespace SynchronizerLibrary.CommonServices
             RapName = name;
         }
     }
-
-
 }
 
